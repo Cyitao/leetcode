8eb68fd79867ffc5aaeb92edde143282db5e1b2e
Cyitao <418867269@qq.com>
2016-07-22 23:28:50 +0800

    2016/7/22  Fifth.(Two files.) Signed-off-by: Cyitao <418867269@qq.com>

---
+++ Longest Palindromic Substring.cpp
@@ -0,0 +1,64 @@
+﻿/*/**
+ * Author: 常奕涛
+ * Date: 2016/07
+ * Github: https://github.com/Cyitao
+ * Declaration: All Rights Reserved !!!
+ **/
+//方法一：
+class Solution {
+public:
+	string longestPalindrome(string s) 
+	{
+		string res;
+		if(s.empty())
+			return res;
+		int n=s.length();
+		for(int i=0;i<2*n-1;i++)
+		{
+			int left=i/2,right=i/2;
+			if(i%2==1)
+				right++;
+			string temp=longestPalindromeCore(s,left,right);
+			if(temp.length()>res.length())
+				res=temp;
+		}
+		return res;
+	}
+	string longestPalindromeCore(string s,int left,int right)
+	{
+		while(left>=0&&right<s.length())
+		{
+			if(s[left]!=s[right])
+				break;
+			left--;
+			right++;
+		}
+		left++;
+		right--;
+		return s.substr(left,right-left+1);
+	}
+};
+//方法二：
+class Solution {
+public:
+	string longestPalindrome(string s) 
+	{
+		string res;
+		if(s.empty())
+			return res;
+		vector<vector<bool> > Is_Pali(s.length(),vector<bool>(s.length(),false));
+		int max=0,pos=0;
+		for(int i=s.length()-1;i>=0;i--)
+			for(int j=i;j<s.length();j++)
+				if(s[i]==s[j]&&(j-i<=2||Is_Pali[i+1][j-1]))
+				{
+					Is_Pali[i][j]=true;
+					if(j-i+1>max)
+					{
+						max=j-i+1;
+						pos=i;
+					}
+				}
+				return s.substr(pos,max);
+	}
+};
---
+++ Max Points on a Line.cpp
@@ -0,0 +1,45 @@
+﻿/*/**
+ * Author: 常奕涛
+ * Date: 2016/07
+ * Github: https://github.com/Cyitao
+ * Declaration: All Rights Reserved !!!
+ **/
+
+
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPoints(vector<Point>& points) 
+    {
+        if(points.size()<=2||allSame(points))
+             return points.size();
+        int res=0;
+        for(int i=0;i<points.size();i++)
+           for(int j=i+1;j<points.size();j++)
+           {
+               if(points[i].x==points[j].x&&points[i].y==points[j].y)
+                    continue;
+               int cur=2;
+               for(int k=0;k<points.size();k++)
+                    if(k!=i&&k!=j&&(points[k].y-points[i].y)*(points[i].x-points[j].x)==(points[i].y-points[j].y)*(points[k].x-points[i].x))
+                          cur++;
+               res=max(res,cur);
+           }
+        return res;
+    }
+    bool allSame(vector<Point>& points)
+    {
+        for(int i=0;i<points.size()-1;i++)
+          if(points[i].x!=points[i+1].x||points[i].y!=points[i+1].y)
+              return false;
+        return true;
+    }
+};
